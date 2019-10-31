85d1eb55747df96132fd465d80a961ad207ae557
howardg1 <howardg@hartwick.edu>
2019-10-31 08:54:23 -0400

    beginning + 1

--- Wick_Dungeon.py
+++ Wick_Dungeon.py
@@ -9,5 +9,8 @@ def roll_dice():
     roll = random.randint(2, 12)
     return roll
 
+
 def create():
     print("Welcome to the Hartwick Dungeon!")
+    print("Would you like to play as a Warrior or a Sorcerer?")
+    print()
