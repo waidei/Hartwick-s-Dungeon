5990676d976ed80453d330cd3afa05ae9f9b0b18
howardg1 <howardg@hartwick.edu>
2019-10-31 08:48:17 -0400

    beginning

---
+++ Wick_Dungeon.py
@@ -0,0 +1,13 @@
+# Gavin & Isaac
+# 10/31/19
+# Hartwick Dungeon
+
+import random
+
+
+def roll_dice():
+    roll = random.randint(2, 12)
+    return roll
+
+def create():
+    print("Welcome to the Hartwick Dungeon!")
